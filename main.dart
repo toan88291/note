d7c42de21baf4d0b877edf60b5dfd4e0b29c1835
toanagiletech <58019369+toanagiletech@users.noreply.github.com>
2020-06-03 15:07:05 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,4 @@
+///////////////// Convert time ////////////////////////////////
 String formatTimeOfDay(TimeOfDay tod) {
     final dt = DateTime(tod.hour, tod.minute);
     final format = DateFormat.jm(); ////////// "06:30 AM"
@@ -6,4 +7,34 @@ String formatTimeOfDay(TimeOfDay tod) {
 
 intl: ^0.16.1
 
-famtech 
+/////////////////  Live Template StateFulWidget //////////////
+    
+import 'package:flutter/material.dart';
+
+class $NAME$ extends StatefulWidget {
+  @override
+  _$NAME$State createState() => _$NAME$State();
+}
+
+class _$NAME$State extends State<$NAME$> {
+  @override
+  Widget build(BuildContext context) {
+    return Container($END$);
+  }
+}
+
+- capitalize(underscoresToCamelCase(fileNameWithoutExtension()))
+    
+///////////////. Live Template StateLessWidget //////////////
+    
+import 'package:flutter/material.dart';
+
+class $NAME$ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+- capitalize(underscoresToCamelCase(fileNameWithoutExtension()))
+///////////////////////////////////////////////////////////////
+
